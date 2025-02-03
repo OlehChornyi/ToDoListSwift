1e6c7e0dd48fb894118711dd18a344761bab8207
oleh <phd.chornyi@gmail.com>
2025-02-03 10:41:16 +0200

    Initial Commit

---
+++ ToDoListSwift/ToDoListSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftApp.swift
+//  ToDoListSwift
+//
+//  Created by Oleh on 03.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
